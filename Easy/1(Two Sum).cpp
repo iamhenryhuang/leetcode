931ed4b79ecbc2d7a246f928ea7f5dcc28baa319
Henry Huang <136736359+iamhenryhuang@users.noreply.github.com>
2024-06-25 12:01:58 +0800

    Create 1(Two Sum).cpp

---
+++ Easy/1(Two Sum).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+        return {};  // Return empty vector if no solution is found
+    }
+};
+
+// solution -> hash map
+開一個complement去找跟target到底還差多少
