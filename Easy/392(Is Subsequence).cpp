76c59a15a0cf508353bcdc6948ee87dffd98dede
Henry Huang <136736359+iamhenryhuang@users.noreply.github.com>
2024-06-26 21:05:36 +0800

    Create 392(Is Subsequence).cpp

---
+++ Easy/392(Is Subsequence).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int s_index = 0, t_index = 0;
+        
+        while (s_index < s.size() && t_index < t.size()) {
+            if (s[s_index] == t[t_index]) {
+                s_index++;
+            }
+            t_index++;
+        }
+
+        // if s is on its last position
+        return s_index == s.size();
+    }
+};
