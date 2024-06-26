c89eb64fe9f89117cbbe5e7eec0ca2672ebd030a
Henry Huang <136736359+iamhenryhuang@users.noreply.github.com>
2024-06-26 21:24:03 +0800

    Update 7.(Climbing Stairs).cpp

--- Easy/7.(Climbing Stairs).cpp
+++ Easy/7.(Climbing Stairs).cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        // 初始化記憶化數組，大小為 n + 1，所有元素初始值為 -1
         vector<int> memo(n + 1, -1);
         return climbStairsHelper(n, memo);
     }
     
     int climbStairsHelper(int n, vector<int>& memo) {
-        // 基本情況：如果 n <= 2，直接返回 n
         if (n <= 2) {
             return n;
         }
-        // 如果已經計算過 memo[n]，則直接返回該值
         if (memo[n] != -1) {
             return memo[n];
         }
-        // 計算 climbStairs(n - 1) 和 climbStairs(n - 2) 並存儲在 memo[n] 中
         memo[n] = climbStairsHelper(n - 1, memo) + climbStairsHelper(n - 2, memo);
         return memo[n];
     }
 };
+
+// solution
+這是一個費式數列的問題，用遞迴去解，但因為會超時，所以需要使用帶記憶化的遞迴方法
